5d752233c482e4577467c174d82b23f89ab363a4
Rex McAllister <45571382+rex-mcall@users.noreply.github.com>
2020-07-16 15:02:17 -0400

    added the directory generator

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,8 +1,19 @@
+import os
 import pyqrcode
-import png
 from pyqrcode import QRCode
+
+# Makes the directory called "generatedCodes" if it doesn't already exist
+dirItems = os.listdir() #creates an array of all folders and files in the current working directory
+dirExists = False
+for x in dirItems:
+    if x == "generatedCodes":
+        dirExists = True
+if dirExists:
+    pass
+else:
+    os.mkdir("generatedCodes") #creates the new directory
 
 urlInput = input("Enter the url: ")
 url = pyqrcode.create(urlInput)
 name = input("What would you like to call it: ")
-url.png('qrcodes/' + name + '.png', scale = 6)
+url.png('generatedCodes/' + name + '.png', scale = 6)
